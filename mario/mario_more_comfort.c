a5e433361582349665d1e7c00a26176106e5ebe5
ulaurent <39177018+ulaurent@users.noreply.github.com>
2018-11-27 17:44:30 -0500

    Update mario_more_comfort.c
    stylistic updates

--- mario/mario_more_comfort.c
+++ mario/mario_more_comfort.c
@@ -1,49 +1,47 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void){
+int main(void)
+{
+    int height = -1;
 
-
-// Variables for the height the user inputs
-// Blocks and spaces variables to build the pyramid
-int height=-1;
-int blocks;
-int spaces;
-int blocks2;
-
-// A while loop to only accept user inputs betwwen 0 and 23
-// if not, continue prompting the user until correct input
-while(height < 0 || height > 23){
-    height = get_int("Height: ");
-}
-
-printf("your height is, %i\n",height);
-
-// constructed forloop to iterate through, starting at 1 and as long as the int
-// is less then the user inputs height
-for (int i = 1; i < height; i++){
-    // another constructed forloop to genearte spaces, spaces beginning at height-1 , and always
-    // has to be greater than or equal to the i value
-    for (spaces = height-1; spaces >= i; spaces--){
-        printf(" ");
-    }
-    //constructed for loop to generate the amount of x's needed per line;
-    // blocks starts at zero and increases as long as its less than or equal to i
-    for (blocks = 0; blocks <= i; blocks ++){
-        printf("x");
+    //Ask user for a height between 0 and 23
+    while (height < 0 || height > 23)
+    {
+        height = get_int("Height: ");
     }
 
-    // prints spaces between the blocks of each pyramid line
-    printf(" ");
+    //for loop will print each row
+    for (int i = 1; i <= height; i++)
+    {
+        //Determine span of pyramid
+        int span = (height * 2) + 2;
 
-    //constructed for loop to generate the amount of x's needed per line;
-    // blocks starts at zero and increases as long as its less than or equal to i
-    for (blocks2 = 0; blocks2 <= i; blocks2 ++){
-        printf("x");
+        //for loop to print chracters at each index of the row
+        for (int j = 0; j < span; j++)
+        {
+            //First half pyramid boundaries
+            if ((height - i) <= j && j < height)
+            {
+                printf("#");
+            }
+            //Second half of pyramid boundaries
+            else if (j > height + 1 && j <= height + 1 + i)
+            {
+                printf("#");
+            }
+            //Set the spaces between the pyramids
+            else if (j <= height + 1)
+            {
+                printf(" ");
+            }
+        }
+        //Next row on a new line before loop continues
+        printf("\n");
     }
 
-// new line command to begin new line before the first forloop adds ++1 to i
-printf("\n");
 }
 
-}
+
+
+
